e7a05e296d17281f82092f7e98decfc36ba24efe
Samuel Issadore <94148426+theutisticcoder@users.noreply.github.com>
2025-10-18 07:31:24 -0400

    fix express

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 app.use(express.json());
-
+app.use(express.static(__dirname)); // Serve your HTML file from a 'public' directory
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
+
 app.post('/generate-speech', async (req, res) => {
     const text = req.body;
     console.log(text);
@@ -38,7 +39,6 @@ app.post('/generate-speech', async (req, res) => {
         res.status(500).send('Error generating speech.');
     }
 });
-app.use(express.static(__dirname)); // Serve your HTML file from a 'public' directory
 
 app.listen(port, () => {
     console.log(`Server listening at port ${port}`);
