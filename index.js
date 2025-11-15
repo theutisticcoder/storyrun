3e7d59add6fe2d05f9e884c66523646ebe2232eb
Samuel Issadore <94148426+theutisticcoder@users.noreply.github.com>
2025-11-15 16:24:22 -0500

    fox speech

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const cors = require("cors")
 app.use(express.text());
 app.use(cors()); // Allow all origins, or configure specific origins
-app.post('/api/generate-speech', async (req, res) => {
+app.post('/generate-speech', async (req, res) => {
     const text = req.body;
     console.log(text);
     try {
@@ -28,7 +28,5 @@ app.post('/api/generate-speech', async (req, res) => {
         res.status(500).send('Error generating speech.');
     }
 });
-app.listen(port, () => {
-    console.log(`Server listening at port ${port}`);
-});
+module.exports = app;
 
