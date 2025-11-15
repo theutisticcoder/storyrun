90c560a58ba827cb1135cdcb7f480e1f97b419d8
Samuel Issadore <94148426+theutisticcoder@users.noreply.github.com>
2025-11-15 08:13:22 -0500

    fix cors

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const { UniversalEdgeTTS } = require('universal-edge-tts');
 const app = express();
 const path = require("path");
 const port = 3000;
+const cors = require("cors")
 app.use(express.text());
-app.get("/"); 
+app.use(cors()); // Allow all origins, or configure specific origins
+app.get("/");
 app.post('/generate-speech', async (req, res) => {
     const text = req.body;
     console.log(text);
     try {
         const tts = new UniversalEdgeTTS(text, 'en-US-BrianNeural');
         var result = await tts.synthesize();
-        var fname = crypto.pseudoRandomBytes(16).toString("hex")+'-output.mp3'
+        var fname = crypto.pseudoRandomBytes(16).toString("hex") + '-output.mp3'
         // Collect all the audio data chunks
         const audioBuffer = Buffer.from(await result.audio.arrayBuffer());
         fs.writeFile(fname, audioBuffer, (err) => {
